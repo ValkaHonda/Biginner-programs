d17cc6137c754496dac18efee7210f08b432b051
Lenny <valentin805@gmail.com>
2018-12-08 14:06:22 +0200

    Solution for biggestOfThree.

---
+++ JavaScript/ConditionalStatements/biggestOfThree.js
@@ -0,0 +1,27 @@
+const getGets = (arr) => {
+    let index = 0;
+  
+    return () => {
+      const toReturn = arr[index];
+      index += 1;
+      return toReturn;
+    };
+  };
+  
+  // This is the place where you must place your test data
+  const test = [
+    '-0.5',
+    '-10',
+    '0'
+  ];
+  
+  const gets = this.gets || getGets(test);
+  const print = this.print || console.log;
+  
+  // Solution of the problem. All the above code is for local testing. The test variable is with the first test case 2 + 2 = 4
+  const a = Number(gets());
+  const b = Number(gets());
+  const c = Number(gets());
+ 
+  print(Math.max(a,b,c));
+  
