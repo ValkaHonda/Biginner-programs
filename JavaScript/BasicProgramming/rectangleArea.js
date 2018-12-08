733206d7900905716b400cda8d89c1b5e4810f8c
Lenny <valentin805@gmail.com>
2018-12-08 18:48:02 +0200

    Solution for rectangleArea

---
+++ JavaScript/BasicProgramming/rectangleArea.js
@@ -0,0 +1,26 @@
+const getGets = (arr) => {
+    let index = 0;
+  
+    return () => {
+      const toReturn = arr[index];
+      index += 1;
+      return toReturn;
+    };
+  };
+  
+  // This is the place where you must place your test data
+  const test = [
+    '10',
+    '10'
+  ];
+  
+  const gets = this.gets || getGets(test);
+  const print = this.print || console.log;
+  
+  // Solution of the problem. All the above code is for local testing. The test variable is with the first test case 2 + 2 = 4
+//   const price = Number(gets());
+//   const givinMoney = Number(gets());
+const a = Number(gets());
+const b = Number(gets());
+
+  print(a*b);
