5936b0c494e61fab22b9373f402ba31a3fa455da
Lenny <valentin805@gmail.com>
2018-12-08 21:02:05 +0200

    mpg converter.

---
+++ JavaScript/BasicProgramming/converter.js
@@ -0,0 +1,27 @@
+const getGets = (arr) => {
+    let index = 0;
+  
+    return () => {
+      const toReturn = arr[index];
+      index += 1;
+      return toReturn;
+    };
+  };
+  
+  // This is the place where you must place your test data
+  const test = [
+    '20'
+  ];
+  
+  const gets = this.gets || getGets(test);
+  const print = this.print || console.log;
+  
+  // Solution of the problem. All the above code is for local testing. The test variable is with the first test case 2 + 2 = 4
+
+  const mpg = Number(gets());
+  const liter = mpg * 1.6;
+
+  const economy = 100 * 4.54 / liter;
+  print(Math.floor(economy) + ' litres per 100 km')
+
+ 
