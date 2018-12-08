3475cd28c3606c7799a512ca9d9c0aaaa21acce6
Lenny <valentin805@gmail.com>
2018-12-08 18:45:54 +0200

    Solution for hello yourName.

---
+++ JavaScript/BasicProgramming/helloYou.js
@@ -0,0 +1,24 @@
+const getGets = (arr) => {
+    let index = 0;
+  
+    return () => {
+      const toReturn = arr[index];
+      index += 1;
+      return toReturn;
+    };
+  };
+  
+  // This is the place where you must place your test data
+  const test = [
+    'John'
+  ];
+  
+  const gets = this.gets || getGets(test);
+  const print = this.print || console.log;
+  
+  // Solution of the problem. All the above code is for local testing. The test variable is with the first test case 2 + 2 = 4
+//   const price = Number(gets());
+//   const givinMoney = Number(gets());
+const name = gets();
+
+  print(`Hello, ${name}!`);
