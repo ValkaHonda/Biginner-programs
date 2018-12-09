183af32c6d8d5ef75f0b07ccbf728d4439bc5095
Lenny <valentin805@gmail.com>
2018-12-09 17:52:31 +0200

    Not complete ComplexSum.

---
+++ JavaScript/loops/CalculateComplexSum.js
@@ -0,0 +1,40 @@
+const getGets = (arr) => {
+    let index = 0;
+  
+    return () => {
+      const toReturn = arr[index];
+      index += 1;
+      return toReturn;
+    };
+  };
+  
+  // This is the place where you must place your test data
+  const test = [
+    '3',
+    '2'
+  ];
+  
+  const gets = this.gets || getGets(test);
+  const print = this.print || console.log;
+  
+  // Solution of the problem. All the above code is for local testing. The test variable is with the first test case 2 + 2 = 4
+  let n = Number(gets());
+  let x = Number(gets());
+
+  let result = 1;
+
+  for(let i = 1; i <= n; i++){
+      let factorial = Number(calculateFactorial(i));
+      let temp = (factorial / (x*i));
+      result += temp;
+  }
+
+  print(result.toFixed(5));
+
+  function calculateFactorial(number){
+    let factorial = 1;  
+    for(let i = 1; i <= number; i++){
+        factorial *= i;
+      }
+      return factorial;
+  }
