d34c5fc355c27c13bf9829604cc1cef8940a3320
Lenny <valentin805@gmail.com>
2018-12-14 12:50:34 +0200

    Combine two lists of numbers.

--- JavaScript/arrays/combineLists.js
+++ JavaScript/arrays/combineLists.js
@@ -10,7 +10,8 @@ const getGets = (arr) => {
   
   // This is the place where you must place your test data
   const test = [
-    '2, 3, 1, 5, 6',
+    '2,3,1',
+    '5,2,3'
   ];
   
   const gets = this.gets || getGets(test);
@@ -18,16 +19,30 @@ const getGets = (arr) => {
   
   // Solution of the problem. All the above code is for local testing. The test variable is with the first test case 2 + 2 = 4
 
-  let arr = [];
-  let inputArr = gets().split(', ');
-  
+  let arr1 = [];
+  let inputArr = gets().split(',');
+
   for(let i = 0; i < inputArr.length; i++){
-      arr.push(Number(inputArr[i]));
+      arr1.push(Number(inputArr[i]));
+  }
+
+  let arr2 = [];
+  inputArr = gets().split(',');
+
+  for(let i = 0; i < inputArr.length; i++){
+    arr2.push(Number(inputArr[i]));
   }
   
   let result = '';
-  for(let i = arr.length-1; i > 0; i--){
-      result += arr[i] + ', ';  
+  for(let i = 0; i < arr1.length-1; i++){
+    result += arr1[i] + ',' +  arr2[i] + ',';
   }
-  result += arr[0];
+  result += arr1[arr1.length-1] + ',' + arr2[arr2.length-1];
+
   print(result);
+
+  
+
+
+
+  
